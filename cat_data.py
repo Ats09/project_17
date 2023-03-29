96bd8a176b8199e0c93acbd109c693972e232dea
Ats09 <acnologia09@yandex.ru>
2023-03-29 20:08:12 +0300

    Initial commit

---
+++ cat_data.py
@@ -0,0 +1,7 @@
+from cat import Cat
+
+cat1 = Cat("БАРОН", "МАЛЬЧИК", 2)
+cat2 = Cat("СЭМ", "МАЛЬЧИК", 2)
+
+print(f"Имя: {cat1.name}, пол: {cat1.gender}, возраст: {cat1.age}")
+print(f"Имя: {cat2.name}, пол: {cat2.gender}, возраст: {cat2.age}")
