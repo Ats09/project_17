08ced4351302c7e5ce9d53acc95495dbc87a2602
alex <115920381+Ats09@users.noreply.github.com>
2023-03-31 17:22:15 +0300

    Delete 123.py

--- 123.py
+++
@@ -1,5 +0,0 @@
-import math
-n = 10000
-print(n ** 2 / (n * math.log(n, 2)))
-def bmi(weight, height):
-    return "Underweight" if bmi <= 18.5 elif <= 25.0 "Normal" elif <= 30.0 "Overweight" else "Obese"
