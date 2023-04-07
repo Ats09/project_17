1e2a2de1681315c20f7e92c55d0520ae6dd99751
alex <115920381+Ats09@users.noreply.github.com>
2023-04-07 16:00:01 +0300

    Delete cat.py

--- cat.py
+++
@@ -1,8 +0,0 @@
-class Cat:
-    def __init__(self, name, gender, age):
-        self.name = name
-        self.gender = gender
-        self.age = age
-
-    def get_cats(self):
-        return f"Имя: {self.name}, пол: {self.gender}, возраст: {self.age}"
